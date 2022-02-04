83c04dc8ebfabfba6ac64ebbb1992089146e55b3
Vladimir <mkking@gmail.com>
2022-02-04 11:55:28 +0300

    start UniqueWordsKeeper

---
+++ src/main/java/com/epam/training/student_Uladzimir_Vinnik/collections/optional_task/UniqueWordsKeeper.java
@@ -0,0 +1,39 @@
+package com.epam.training.student_Uladzimir_Vinnik.collections.optional_task;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+public class UniqueWordsKeeper {
+    private static final String FILE_PATH = "src/test/resources/FileWithEnglishText.text";
+
+    private static String getDataFromTheFile(String filePath) throws IOException {
+        StringBuilder stringBuilder = new StringBuilder();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))){
+
+            while(bufferedReader.ready()) {
+                stringBuilder.append(bufferedReader.readLine());
+            }
+            return stringBuilder.toString();
+        }
+    }
+
+    private static Set<String> convertToSet(String string){
+        Set<String> set = new HashSet<>();
+        if(string != null){
+            set.addAll(Arrays.asList(string.replace("\\.\\,","").split(" ")));
+        }
+        return set;
+    }
+
+    private static void printList(Collection<String> list) {
+        list.forEach((s) -> System.out.printf("%s  ", s));
+    }
+    public static void main(String[] args) throws IOException {
+
+
+        printList(convertToSet(getDataFromTheFile(FILE_PATH)));
+
+    }
+}
