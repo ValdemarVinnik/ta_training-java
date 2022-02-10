d9220472002fe4572dfd4f18836585d958a796b2
Vladimir <mkking@gmail.com>
2022-02-10 16:07:56 +0300

    DownloaderParkFromFile started.

---
+++ src/main/java/com/epam/training/student_Uladzimir_Vinnik/collections/taxi_park/action/DownloaderParkFromFile.java
@@ -0,0 +1,52 @@
+package com.epam.training.student_Uladzimir_Vinnik.collections.taxi_park.action;
+
+import com.epam.training.student_Uladzimir_Vinnik.collections.taxi_park.cars.Transport;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DownloaderParkFromFile {
+    private static final String FILE_PATH = "src/test/resources/TaxiPark.txt";
+
+    private static List<String> getDataFromTheFile(String filePath) throws IOException {
+        List<String> list = new ArrayList<>();
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath))) {
+
+            while (bufferedReader.ready()) {
+                list.add(bufferedReader.readLine());
+            }
+            return list;
+        }
+    }
+
+    public static List<Transport> getTaxiFromFile(){
+        return null;
+    }
+
+    private static Transport createTransport(String[] dataArray){
+        String typeOfTransport = dataArray[5].trim().toUpperCase();
+        switch (typeOfTransport){
+            case ("SEDAN"):return null;
+            case ("CARGOBUS"):return null;
+            case ("PASSENGER_BUS"):return null;
+            case ("TRACK"):return null;
+            case ("MINIVAN"):return null;
+            default: return null;
+
+
+        }
+    }
+
+    private static Transport getDeserializableCar(String data){
+        if (data == null){return null;}
+        String[] dataArray = data.split(",");
+        if (dataArray.length != 7) {return null;}
+
+
+        return null;
+    }
+
+}
